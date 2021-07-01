eeeea65fdc67c0678a1bf3f8d2d9e4ad2b1fedd7
tuyen21 <86776880+tuyen21@users.noreply.github.com>
2021-07-01 18:08:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnIntro: 'nhấn vào đây',
+    title: 'Anh muốn có một nóc nhà siêu cấp đáng iu là em chứ ? 😙',
+    desc: 'Anh đồng ý khônggg ? ',
+    btnYes: 'Yebbb <33',
+    btnNo: 'No, sorry babee :3',
+    question:'Trên thế giới hơn 7 tỉ người, vậy tại sao anh lại yêu em <3',
+    btnReply: 'Send me <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Em cũng dịi 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Together forever',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100008710418297'
 }
